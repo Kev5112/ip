0c8ac7e766da1c829838ab144318f8747f28af49
Fred <kevinbenzlim@gmail.com>
2026-01-23 00:50:32 +0800

    Change name and greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# AlterEgo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/AlterEgo.java
@@ -0,0 +1,11 @@
+public class AlterEgo {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + " Hello! I'm Alter Ego\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println("Hello from\n" + logo);
+    }
+}
