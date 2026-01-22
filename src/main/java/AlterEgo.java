6480132459be8fcb63afa3d008a70d518b36edfe
Fred <kevinbenzlim@gmail.com>
2026-01-23 01:15:08 +0800

    Level-1 Echo is done

--- src/main/java/AlterEgo.java
+++ src/main/java/AlterEgo.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class AlterEgo {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        Scanner scanner = new Scanner(System.in);
+        String greeting = "____________________________________________________________\n"
                 + " Hello! I'm Alter Ego\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        String farewell = " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(greeting);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("bye")) break;
+            System.out.println(line + "\n " + input + "\n" + line + "\n");
+        }
+        System.out.println(farewell);
     }
 }
